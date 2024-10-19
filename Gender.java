eb361ebcd90f9327e167d800a48f3ff98ab04b4d
Adilson Ribeiro <aguivari.ie@gmail.com>
2024-10-19 21:02:43 +0100

    Added labels to some enums, put them on own java files. Makefile adjustments

---
+++ Gender.java
@@ -0,0 +1,11 @@
+public enum Gender {
+    MALE("Male"),
+    FEMALE("Female"),
+    UNDEFINED("Undefined");
+
+    public final String label;
+    private Gender(String label) {
+        this.label = label;
+    }
+
+}
